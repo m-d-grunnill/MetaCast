7ee605eabd2e2027507dbe620ea62311889df89a
Martin Grunnill <47453723+m-d-grunnill@users.noreply.github.com>
2024-04-01 11:42:22 -0400

    Removed confy.py in root directory.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'MetaCast'
 copyright = '2024, Martin Grunnill'
 author = 'Martin Grunnill'
-release = '0.0.1'
+release = '0.1.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

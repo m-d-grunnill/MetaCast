a1f3a1f8523ba4b16c08c92fb6ad843d550e0fd1
Martin Grunnill <47453723+m-d-grunnill@users.noreply.github.com>
2024-04-01 12:05:55 -0400

    Changed line 30 of docs/source/conf.py to `autoapi_dirs = ['src']`.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ extensions = ['autoapi.extension',
 templates_path = ['_templates']
 exclude_patterns = []
 
-autoapi_dirs = ['../src']
+autoapi_dirs = ['src']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
